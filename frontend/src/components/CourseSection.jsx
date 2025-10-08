13ffb87e6bd7436ea6a77c07a1e96cee7de4f690
juanMS <285juanda@gmail.com>
2025-10-08 02:27:01 -0500

    Mejora apariencia visual de la sección de explorar cursos

--- frontend/src/components/CourseSection.jsx
+++ frontend/src/components/CourseSection.jsx
@@ -1,10 +1,20 @@
+import { Star } from "../assets/AppIcons";
 import "../styles/CourseSection.css";
 import CourseCard from "./CourseCard";
 
-export default function CourseSection({ title, courses }) {
+export default function CourseSection({ title, courses, isFirst }) {
   return (
     <section className="course-section">
-      <h2>{title}</h2>
+
+      <div className="decoration">
+        {
+          isFirst &&
+          <Star />
+        }
+
+        <h2>{title}</h2>
+      </div>
+
       <div className="course-grid">
         {courses.map((course, i) => (
           <CourseCard key={i} {...course} />
